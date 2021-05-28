15afef85ca63482e20688e7700826582c7f24d7a
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2021-05-28 20:09:38 +0600

    project starter

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyADHzV2wtx37eVw8-i3Go1A74qiIa_oogQ",
+    authDomain: "challenge-3ebb3.firebaseapp.com",
+    projectId: "challenge-3ebb3",
+    storageBucket: "challenge-3ebb3.appspot.com",
+    messagingSenderId: "1048797201375",
+    appId: "1:1048797201375:web:810fb1340306c6b809fa6a",
+    measurementId: "G-QM2R0HNPZX"
+  };
